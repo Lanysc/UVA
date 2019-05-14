2dc28d394e7efbdc5e5c0c719c71d941c8b07bfd
Lanysc <36964927+Lanysc@users.noreply.github.com>
2019-05-14 16:16:01 -0300

    Update uva.cpp

--- uva.cpp
+++ uva.cpp
@@ -4,43 +4,51 @@ using namespace std;
 
 int main()
 {
-    int long long num, matrix[4][8], cont = 0, aux = 1, conv = 1, NumA;
-    scanf("%lld",&num);
-    NumA = num;
-    for (int i = 0; i < 4; i++)
+    int long long num, matrix[4][8], cont = 0, NumA, soma,sinal, pot;
+    while(scanf("%lld",&num) == 1)
     {
-        for (int j = 0; j < 8; j++)
-        {
+		sinal = 1;
+		soma = 0;
+		NumA = num;
+		cont = 0;
+		pot = 1;
+		if (num < 0)
+		{
+			sinal = -1;
+			num = abs(num);
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				if (num / 2 != 0 || num == 1)
+				{
+					matrix[i][j] = num % 2;
+					num = num / 2;
+				}
+				else
+				{
+					matrix[i][j] = 0;
+				}
+				cout << matrix[i][j];
+			}
+			puts ("");
+		}
+		puts ("");
+		for (int i = 3; i >= 0; i--)
+		{
+			for (int j = 0; j < 8; j++,cont++)
+			{
+				cout << matrix[i][j];
+				if (matrix[i][j] != 0)
+				{
+					soma += pot;
+				}
+				pot = pot * 2;
+			}
+			puts("");
+		}
 
-            if (num / 2 != 0 || num == 1)
-            {
-                matrix[i][j] = num % 2;
-                num = num / 2;
-            }
-            else
-            {
-                matrix[i][j] = 0;
-            }
-        }
-
-    }
-    for (int i = 3; i >= 0; i--)
-    {
-        for (int j = 0; j < 8; j++,cont++)
-        {
-            if (matrix[i][j] != 0)
-            {
-                for (int k = aux; k <= cont; k++)
-                {
-                    conv = conv * 2;
-                }
-                aux = cont;
-
-            }
-        }
-
-    }
-
-    printf("%lld converts to %lld",NumA,conv);
-
+		printf("%lld converts to %lld\n",NumA,soma * sinal);
+	}
 }
