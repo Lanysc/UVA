cfe3640b4dbeae7c846cf5042a7f20b93fd82182
Lanysc <36964927+Lanysc@users.noreply.github.com>
2019-05-22 20:17:55 -0300

    Update 10067.cpp

--- 10067.cpp
+++ 10067.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-vector<Whells> Proibido;
-
 struct Whells
 {
     int v1, v2, v3, v4;
@@ -23,34 +21,66 @@ struct Whells
             return true;
         return false;
     }
+    
+    bool operator != (Whells b)
+    {
+        if(v1 != b.v1 && v2 != b.v2 && v3 != b.v3 && v4 != b.v4)
+            return true;
+        return false;
+    }
 };
+
+queue<Whells> Qu;
+vector<Whells> Proibido;
+int A_[8] = {1,-1,0,0,0,0,0,0},
+    B_[8] = {0,0,1,-1,0,0,0,0},
+    C_[8] = {0,0,0,0,1,-1,0,0},
+    D_[8] = {0,0,0,0,0,0,1,-1},
+    botoes, node, nodeT;
 
 bool verificar (Whells a)
 {
-    int i;
-    for (i = 0;Whells a != Whells b && i < (int)Proibidos.size(); i++);
-    if (i < (int)Proibidos.size())
-        return false
-    return true
+    int i, size;
+    size = (int)Proibido.size();
+    for (i = 0;a != Proibido[i] && i < size; i++);
+    if (i < (int)Proibido.size())
+        return false;
+    return true;
 }
 
 void bfs(int a, int b, int c, int d)
 {
-
+    for (int i = 0; i < 8; i++)
+    {
+        a = (abs(a) + A_[i]) % 10;
+        b = (abs(b) + B_[i]) % 10;
+        c = (abs(c) + C_[i]) % 10;
+        d = (abs(d) + D_[i]) % 10;
+        printf("%d %d %d %d\n",a,b,c,d);
+        if (verificar(Whells(a,b,c,d)))
+        {
+            puts ("entrei");
+            Qu.push(Whells(a,b,c,d));
+            Proibido.push_back(Whells(a,b,c,d));
+            nodeT++;
+        }
+    }
 }
 
 int main()
 {
-    queue<Whells> Qu;
-    int test, Var1[4], Var2[4];
+    int test, Var1[4], Var2[4], VarR[4];
     scanf("%d",&test);
     while(test--)
     {
         int Pb;
-        bool ok;
+        botoes = 0;
+        node = 0;
+        nodeT = 0;
+        bool ok = false;
         scanf("%d %d %d %d", &Var1[0], &Var1[1], &Var1[2], &Var1[3]);
-        Qu.push(Whells(a, b, c, d));
-        scanf("%d %d %d %d", &Var1[0], &Var1[1], &Var1[2], &Var1[3]);
+        Qu.push(Whells(Var1[0], Var1[1], Var1[2], Var1[3]));
+        scanf("%d %d %d %d", &VarR[0], &VarR[1], &VarR[2], &VarR[3]);
         scanf("%d",&Pb);
         while(Pb--)
         {
@@ -59,8 +89,30 @@ int main()
         }
         while(!ok && !Qu.empty())
         {
-			
+			int a = Qu.front().v1;
+            int b = Qu.front().v2;
+            int c = Qu.front().v3;
+            int d = Qu.front().v4;
+            printf("%d %d %d %d\n",a,b,c,d);
+            Qu.pop();
+            if (a == VarR[0] && b == VarR[1] && c == VarR[2] && d == VarR[3])
+                ok = true;
+            if (!ok)
+            {
+                bfs(a,b,c,d);
+                if (node <= 0)
+                {
+                    node = nodeT;
+                    botoes++;
+                }
+            }
 		}
+        if (ok)
+            printf("%d\n",botoes);
+        else 
+            puts("-1");
+        while(!Qu.empty())
+            Qu.pop();
+        Proibido.clear();
     }
-
 }
